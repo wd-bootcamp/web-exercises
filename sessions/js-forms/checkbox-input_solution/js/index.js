5e24a69f64905e41328d62b3ccc9504af2378073
Jessica Lörs <jessica.loers@gmail.com>
2024-12-09 19:30:05 +0100

    update solution with bonus challenge and optional dynamically error message on change

--- sessions/js-forms/checkbox-input_solution/js/index.js
+++ sessions/js-forms/checkbox-input_solution/js/index.js
@@ -1,8 +1,18 @@
-console.clear();
-
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+
+// --v-- Bonus: Success message! --v--
+const successMessageElement = document.querySelector('[data-js="success"]');
+
+function hideSuccess() {
+  successMessageElement.setAttribute("hidden", "");
+}
+
+function showSuccess() {
+  successMessageElement.removeAttribute("hidden");
+}
+// --^-- Bonus: Success message! --^--
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,20 +22,39 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+hideTosError();
+// --v-- Bonus: Success message! --v--
+hideSuccess();
+// --^-- Bonus: Success message! --^--
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // --v-- write your code here --v--
-  const tosChecked = tosCheckbox.checked;
-
-  if (!tosChecked) {
+  if (!tosCheckbox.checked) {
     showTosError();
+    // --v-- Bonus: Success message! --v--
+    hideSuccess();
+    // --^-- Bonus: Success message! --^--
     return;
   }
 
   hideTosError();
-  // --^-- write your code here --^--
+  // --v-- Bonus: Success message! --v--
+  showSuccess();
+  // --^-- Bonus: Success message! --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+});
+
+/* Optional:
+This is an example of how to dynamically show or hide the error message
+based on the user's actions (checking or unchecking the box). */
+
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
 });
