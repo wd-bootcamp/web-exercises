211b7c3b763f66ce68f9763b1e420d0cf7f05b17
Jessica Lörs <jessica.loers@gmail.com>
2024-12-09 14:19:22 +0100

    fix react demo - commit issue

--- sessions/react-basics/demo-start/src/index.jsx
+++ sessions/react-basics/demo-start/src/index.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
+import VanillaApp from "./VanillaApp.js";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,5 +10,10 @@ root.render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
+
+//----------------------------------------------------------------------
+// simulated Vanilla JS environment
+const vanillaJSRootElement = document.querySelector("#vanilla-js-root");
+vanillaJSRootElement.append(VanillaApp());
 
