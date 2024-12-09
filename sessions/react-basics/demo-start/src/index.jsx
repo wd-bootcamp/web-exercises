3316318dc7ad4d596c4a5a636f1c9fa14681e83c
Jessica Lörs <jessica.loers@gmail.com>
2024-12-09 14:44:48 +0100

    remove vanilla envioment

--- sessions/react-basics/demo-start/src/index.jsx
+++ sessions/react-basics/demo-start/src/index.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
-import VanillaApp from "./VanillaApp.js";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -11,9 +10,3 @@ root.render(
     <App />
   </StrictMode>
 );
-
-//----------------------------------------------------------------------
-// simulated Vanilla JS environment
-const vanillaJSRootElement = document.querySelector("#vanilla-js-root");
-vanillaJSRootElement.append(VanillaApp());
-
