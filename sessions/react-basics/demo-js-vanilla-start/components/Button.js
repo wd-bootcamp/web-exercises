e5a9aa2966f1fca6547fc8806c2eb5d1d2a0001c
Jessica Lörs <jessica.loers@gmail.com>
2024-12-09 17:43:34 +0100

    simplify by removing the button component

--- sessions/react-basics/demo-js-vanilla-start/components/Button.js
+++
@@ -1,7 +0,0 @@
-export function Button() {
-  const button = document.createElement("button");
-  button.type = "button";
-  button.textContent = "click with Vanilla JS";
-  button.addEventListener("click", () => console.log("Hello Vanilla JS Land"));
-  return button;
-}
