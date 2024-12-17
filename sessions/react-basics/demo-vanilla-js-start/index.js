25d640bf796c6977d2d61fd30fb22ca58eea0f4f
Jessica Lörs <jessica.loers@gmail.com>
2024-12-17 11:09:55 +0100

    rename folder

--- sessions/react-basics/demo-vanilla-js-start/index.js
+++ sessions/react-basics/demo-vanilla-js-start/index.js
@@ -4,6 +4,13 @@ function App() {
   const appContainer = document.createElement("main");
   appContainer.classList.add("App");
 
+  const button = document.createElement("button");
+  button.type = "button";
+  button.textContent = "click with Vanilla JS ";
+  button.addEventListener("click", () => console.log("Hello Vanilla JS Land"));
+
+  appContainer.append(button);
+
   return appContainer;
 }
 
