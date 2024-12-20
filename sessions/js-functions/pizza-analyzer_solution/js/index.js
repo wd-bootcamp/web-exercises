48fa3e71b547fcacb9334d47cd3e91916279ef36
Andrea Hohnsen <a.hohnsen@mail.de>
2024-12-20 11:50:46 +0100

    Fit pizza-analyzer challenge solution to challenge instructions

--- sessions/js-functions/pizza-analyzer_solution/js/index.js
+++ sessions/js-functions/pizza-analyzer_solution/js/index.js
@@ -8,44 +8,53 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
 
-let diameter1 = pizzaInput1.value;
-let diameter2 = pizzaInput2.value;
-
 pizzaInput1.addEventListener("input", () => {
-  diameter1 = pizzaInput1.value;
-  calculatePizzaGain(diameter1, diameter2);
-  updatePizzaDisplay(pizza1, diameter1);
-  updateOutputColor(diameter1, diameter2);
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  updatePizzaDisplay(pizza1, pizzaSize1);
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  diameter2 = pizzaInput2.value;
-  calculatePizzaGain(diameter1, diameter2);
-  updatePizzaDisplay(pizza2, diameter2);
-  updateOutputColor(diameter1, diameter2);
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  updatePizzaDisplay(pizza2, pizzaSize2);
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
-// Task 1
+// Task 1: Define the function `calculatePizzaGain` here
 
 function calculatePizzaGain(diameter1, diameter2) {
-  const pizzaRadius1 = (diameter1 / 2) ** 2 * Math.PI;
-  const pizzaRadius2 = (diameter2 / 2) ** 2 * Math.PI;
-  output.innerHTML = Math.round(
-    ((pizzaRadius2 - pizzaRadius1) / pizzaRadius1) * 100
-  );
+  const pizzaArea1 = (diameter1 / 2) ** 2 * Math.PI;
+  const pizzaArea2 = (diameter2 / 2) ** 2 * Math.PI;
+
+  const pizzaGain = ((pizzaArea2 - pizzaArea1) / pizzaArea1) * 100;
+
+  output.textContent = Math.round(pizzaGain);
 }
 
-// Task 2
+// Task 2: Define the function `updatePizzaDisplay` here
 
 function updatePizzaDisplay(pizzaElement, newSize) {
-  const result = (newSize / 24) * 100;
-  pizzaElement.style.width = `${result}px`;
+  const newDisplaySize = (newSize / 24) * 100;
+  pizzaElement.style.width = `${newDisplaySize}px`;
 }
 
-// Task 3
+// Task 3: Define the function `updateOutputColor` here
 
 function updateOutputColor(size1, size2) {
-  outputSection.style.backgroundColor = `var(${
-    size1 <= size2 ? "--green" : "--red"
-  }`;
+  if (size1 <= size2) {
+    outputSection.style.backgroundColor = "var(--green)";
+  } else {
+    outputSection.style.backgroundColor = "var(--red)";
+  }
 }
+
+// Alternative: Task 3
+// function updateOutputColor(size1, size2) {
+//   outputSection.style.backgroundColor = `var(${
+//     size1 <= size2 ? "--green" : "--red"
+//   }`;
+// }
