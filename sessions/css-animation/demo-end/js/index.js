f0da13099b63b27799d1446399d97a97c9282305
Jakob-M-101 <20402329+jakobm101@users.noreply.github.com>
2026-02-11 12:14:32 +0100

    Fix position calculation for CSS animation

--- sessions/css-animation/demo-end/js/index.js
+++ sessions/css-animation/demo-end/js/index.js
@@ -3,7 +3,7 @@ const button = document.querySelector('[data-js="button-position"]');
 let position = 0;
 
 button.addEventListener("click", () => {
-  const newPosition = (position % 3) + 1; //Because 3 positions got defined in CSS
+  const newPosition = (position + 1) % 3; //Because 3 positions got defined in CSS
   button.classList.replace(`position--${position}`, `position--${newPosition}`);
   position = newPosition;
 });
